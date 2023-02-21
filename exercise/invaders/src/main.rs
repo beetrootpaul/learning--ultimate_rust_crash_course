4c16fdb1e8dfd57ea85e161ae759930e553b5bfc
Beetroot Paul <paul@beetrootpaul.com>
2023-02-21 11:54:12 +0100

    invaders – terminal setup

--- exercise/invaders/src/main.rs
+++ exercise/invaders/src/main.rs
@@ -1,8 +1,25 @@
 use std::error::Error;
+use std::io;
+use std::io::Stdout;
+use crossterm::cursor::{Hide, Show};
+use crossterm::{ExecutableCommand, terminal};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+
 use rusty_audio::Audio;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
+    let mut stdout = io::stdout();
+    setup(&mut audio, &mut stdout).expect("should setup");
+
+    audio.play("startup");
+
+    clean_up(audio, stdout).expect("should clean up");
+    Ok(())
+}
+
+// fn setup(mut audio: &Audio, mut stdout: &Stdout) -> Result<(), Box<dyn Error>> {
+fn setup(audio: &mut Audio, stdout: &mut Stdout) -> Result<(), Box<dyn Error>> {
     audio.add("explode", "assets/sounds/explode.wav");
     audio.add("lose", "assets/sounds/lose.wav");
     audio.add("move", "assets/sounds/move.wav");
@@ -10,8 +27,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("startup", "assets/sounds/startup.wav");
     audio.add("win", "assets/sounds/win.wav");
 
-    audio.play("startup");
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    Ok(())
+}
+
+// fn cleanup(mut audio: Audio, mut stdout: Stdout) -> Result<(), Box<dyn Error>>  {
+fn clean_up(audio: Audio, mut stdout: Stdout) -> Result<(), Box<dyn Error>>  {
 
     audio.wait();
+
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
     Ok(())
 }
